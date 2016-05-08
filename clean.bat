3215345032cfa1ca7afd4a2db15266c948ca69cc
Carmelo Salafia <cswi@gmx.de>
2016-05-08 08:00:25 +0000

    3.0.7.2
    git-svn-id: https://svn.code.sf.net/p/myopenlab3/code/trunk@4 648a92f2-f43c-405f-b956-b2c08725b472

---
+++ clean.bat
@@ -0,0 +1,1 @@
+del /s  *._Thumbs.db 
---
+++ start.bat
@@ -0,0 +1,6 @@
+set classpath=%CLASSPATH%".;Y:\java\carmelo's exp Lab 3\Visual Logic 3\dist\lib\looks-2.0.4.jar"
+set classpath=%CLASSPATH%".;Y:\java\carmelo's exp Lab 3\Visual Logic 3\dist\lib\AbsoluteLayout.jar"
+set classpath=%CLASSPATH%".;Y:\java\carmelo's exp Lab 3\Visual Logic 3\dist\lib\swing-layout-1.0.jar"
+
+java VisualLogic/FrameMain "y:\java\carmelo's exp Lab 3\Distribution\Elements"
+cmd
