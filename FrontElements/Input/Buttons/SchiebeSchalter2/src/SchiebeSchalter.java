cc3be67b306210bcdea1e1651284b4f68104ba67
Carmelo Salafia <cswi@gmx.de>
2016-07-24 20:02:04 +0000

    git-svn-id: https://svn.code.sf.net/p/myopenlab3/code/trunk@16 648a92f2-f43c-405f-b956-b2c08725b472

--- FrontElements/Input/Buttons/SchiebeSchalter2/src/SchiebeSchalter.java
+++ FrontElements/Input/Buttons/SchiebeSchalter2/src/SchiebeSchalter.java
@@ -53,8 +53,9 @@ public class SchiebeSchalter extends JVSMain
 
     setPin(0,ExternalIF.C_BOOLEAN,element.PIN_OUTPUT);
     
-    image=element.jLoadImage(element.jGetSourcePath()+"SchiebeSchalter.GIF");
+    image=element.jLoadImage(element.jGetSourcePath()+"SchiebeSchalter.gif");
     setName("Schiebeschalter");
+    
   }
   
 
