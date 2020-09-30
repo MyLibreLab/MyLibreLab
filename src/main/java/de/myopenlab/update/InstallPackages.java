eb8dee1a59aab4f7864e1c91d575514f7c54006b
MaSven <dev@mail.smarquardt.space>
2020-09-30 20:51:52 +0200

    fix for #18
    Usage of generic exception
    
    Signed-off-by: MaSven <dev@mail.smarquardt.space>

--- src/main/java/de/myopenlab/update/InstallPackages.java
+++ src/main/java/de/myopenlab/update/InstallPackages.java
@@ -1,16 +1,19 @@
 package de.myopenlab.update;
 
+import de.myopenlab.update.exception.PackageTransportationException;
+import org.tinylog.Logger;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 import static VisualLogic.Tools.settings;
 
 public class InstallPackages implements Runnable {
 
+    public static final String MYOPENLAB_ = "myopenlab_";
     public frmUpdate owner;
 
     InstallPackages(frmUpdate aThis) {
@@ -22,7 +25,7 @@ public class InstallPackages implements Runnable {
         try {
             try {
 
-                Path myTempDir = Files.createTempDirectory("myopenlab_");
+                Path myTempDir = Files.createTempDirectory(MYOPENLAB_);
                 // owner.log("create tempdir " + myTempDir.toString());
 
                 for (MyTableRow row : owner.list1) {
@@ -45,8 +48,9 @@ public class InstallPackages implements Runnable {
 
                 // owner.log("delete tempdir " + myTempDir.toString());
                 Tools2.deleteFolder(new File(myTempDir.toString()));
-            } catch (Exception ex) {
-                Logger.getLogger(frmUpdate.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.error(ex,"Could not create temp directory {}", MYOPENLAB_);
+
             }
             Thread.sleep(1000);
             owner.log("finished");
@@ -54,7 +58,7 @@ public class InstallPackages implements Runnable {
             owner.initTable2();
             owner.owner.reinitPackage();
         } catch (InterruptedException ex) {
-            Logger.getLogger(InstallPackages.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.error(ex,"Error. Tried to sleep thread");
         }
     }
 
