c73a0a966af8cfc921af0be5f4cf61c40a25dc99
weisj <weisj@arcor.de>
2020-10-06 22:25:34 +0200

    Route persisting of ui components through the settings-api. Restore ui after the app frame has been created. Persistent components should keep this in mind. They shouldn't depend on any of their bounds when restoring.

---
+++ src/main/kotlin/com/github/mylibrelab/settings/GuiStateSettings.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2020 MyLibreLab
+ * Based on MyOpenLab by Carmelo Salafia www.myopenlab.de
+ * Copyright (C) 2004  Carmelo Salafia cswi@gmx.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.github.mylibrelab.settings
+
+import com.github.mylibrelab.lifecycle.AppLifecycleAdapter
+import com.github.mylibrelab.lifecycle.AppLifecycleListener
+import com.github.mylibrelab.settings.api.*
+import com.github.mylibrelab.ui.persistent.PersistenceManager
+import com.github.mylibrelab.ui.persistent.PersistenceNode
+import com.github.mylibrelab.util.andThen
+import com.github.mylibrelab.util.propertyParser
+import com.github.mylibrelab.util.transformerOf
+import com.google.auto.service.AutoService
+
+@AutoService(AppLifecycleListener::class)
+internal class GuiStateAppLifecycleListener : AppLifecycleAdapter() {
+
+    override fun appFrameCreated() {
+        PersistenceManager.INSTANCE.loadState(GuiStateSettings.state)
+    }
+
+    override fun appFrameClosing() {
+        GuiStateSettings.state = PersistenceManager.INSTANCE.saveState()
+    }
+}
+
+@AutoService(SettingsContainerProvider::class)
+class GuiStateSettingsProvider : SingletonSettingsContainerProvider({ GuiStateSettings })
+
+object GuiStateSettings : DefaultSettingsContainer(identifier = "gui_state") {
+
+    internal var state: PersistenceNode = PersistenceNode()
+
+    init {
+        hidden {
+            val stateTransformer = transformerOf(
+                write = PersistenceNode::fromFlattenedMap,
+                read = PersistenceNode::toFlattenedMap,
+            ) andThen propertyParser()
+            persistentProperty(value = ::state, transformer = stateTransformer)
+        }
+    }
+}
