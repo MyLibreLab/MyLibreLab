e4e02352f931b42770c7c66d0a59c37c8792ecb3
weisj <weisj@arcor.de>
2020-10-07 18:53:26 +0200

    Configure sonarlint. Will ignore any legacy code file.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
         idv("com.github.vlsi.crlf", "com.github.vlsi.vlsi-release-plugins")
         idv("com.github.vlsi.gradle-extensions", "com.github.vlsi.vlsi-release-plugins")
         idv("org.beryx.runtime")
+        idv("name.remal.sonarlint")
     }
 }
 
