ed2b1494967edcdece253d3b7159adfc56e1ecdd
weisj <weisj@arcor.de>
2020-10-17 21:33:17 +0200

    Implement custom annotation processor to to ease the creation of more meaningful provider/service annotations. Currently all services need to be annotated using @AutoService(<service class>) which is often not very conceptual. Also for the sake of decoupling views from models the current ServiceLoader api doesn't provide the option to have any generic constraints.
    The first problem is solved by providing the @ServiceSpec(<service class>) meta annotation,
    which can be used to create new service annotations, where the annotated have to be assignable to the given <service class>.
    For general single-use service declaration the @Service(<service class>) annotation replaces @AutoService(<service class>).
    
    The second problem is solved by providing the @ProviderSpec(inputBound, outputBound) meta annotation which can
    be used to create provider annotations. Classes annotated with @ProviderSpec need to implement the ProviderFor<A, B> interface,
    such that A is assignable from inputBound and B is assignable to outputBound.
    These types are checked when requesting the provided value. Request are fulfilled in the following manner:
    
    **Note** that A and B are treated throughout as if type erasure had been applied.
    
    For an object a of type A which request an object of type B a provider which is "assignable" to ProviderFor<? super A, ? extend B>.
    ("assignable" in quotes because java won't actually let one assign one to the other, but assignable is meant in the way that the following is legal:
    ````java
    ProviderFor<? super A, ? extends B> provider = ...
    A a = ...
    B b = provider.provide(a);
    ````
    ).
    If there are multiple candidates which match, then the provider with the most specific inout and the least specific output type is used.
    i.e. if there are type C < B < A (where < means "is subtype of"), and P<T, K> := ProviderFor<T, K> then
    - requesting P<C, A> with P<A, A>, P<B, A> and P<C, A> available will result in P<C, A> being selected.
    - requesting P<A, A> with P<A, A>, P<A, B> and P<A, C> available will result in P<C, A> being selected.
    If there are multiple providers which fulfill the requested specification which cannot be compared
    (e.g. P<A1, B>, P<A2, B> where A1 and A2 both are subtypes of A but neither is a subtype of the other) then a warning is issued and no guarantee is given
    on which provider will be used.
    
    There also is a general @Provider annotation which imposes no bounds on the input and output type.
    
    In contrast to AutoService the used approach is one of an isolating annotation processor which allows for better incremental compilation.
    
    The ServiceManager is responsible for handling provider/service requests and uses the ServiceLoader internally.

---
+++ annotations/src/main/java/com/github/mylibrelab/annotations/ServiceSpec.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 MyLibreLab
+ * Based on MyOpenLab by Carmelo Salafia www.myopenlab.de
+ * Copyright (C) 2004  Carmelo Salafia cswi@gmx.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.github.mylibrelab.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.ANNOTATION_TYPE)
+public @interface ServiceSpec {
+    Class<?> value();
+}
