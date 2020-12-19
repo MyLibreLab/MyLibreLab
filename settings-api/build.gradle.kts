70e67869717c62c487437a0f4e4552cc7ebfd24d
weisj <weisj@arcor.de>
2020-12-19 14:39:04 +0100

    Remove explicit stdlib dependency.

--- settings-api/build.gradle.kts
+++ settings-api/build.gradle.kts
@@ -7,7 +7,5 @@ dependencies {
     compileOnly(project(":mylibrelab-annotations"))
     implementation(project(":mylibrelab-service-manager"))
     implementation(project(":mylibrelab-util"))
-
-    implementation(kotlin("stdlib"))
     implementation("org.tinylog:tinylog-api")
 }
--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
+    `java-library`
     kotlin("jvm")
     kotlin("kapt")
-    `java-library`
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
